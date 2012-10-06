f1f84370ecf569b898d936b672918463c407615d
AutomatedTester <dburns@mozilla.com>
2012-10-06 21:29:40 +0100

    removing build tab until build status stuff available via CORS

--- extension/data/pageupdate.js
+++ extension/data/pageupdate.js
@@ -8,15 +8,10 @@ style.innerHTML += '.travis-ci.repo{margin:0 0 0 8px}';
 var body = window.document.getElementsByTagName('body')[0];
 body.appendChild(style);
       
-/*
-  css.insertRule('.travis-ci:hover{opacity:1;cursor:pointer}', 1);
-  css.insertRule('.travis-ci img{position:relative;left:-53px;-o-transition:all .3s}', 1);
-  css.insertRule('.travis-ci:hover img{left:0}', 1);
-*/
 function isStatusUnknown(img){
-    // Cannot compare actual image data through canvas due to Same Origin Policy
-    return img.width == 95 && img.height == 13;
-  }
+  // Cannot compare actual image data through canvas due to Same Origin Policy
+  return img.width == 95 && img.height == 13;
+}
 
 function insertBuildStatus(el, project, className){
   var img = document.createElement('img');
@@ -49,82 +44,3 @@ if(links.length > 0){
     }
   }
 }
-
-// Adding a build tab
-var buildTab = window.document.createElement('li');
-buildTab.addEventListener('click', function(){
-  var container = window.document.getElementById("js-repo-pjax-container");
-  container.style.display = 'none';
-  var selectedTab = window.document.querySelectorAll('a.selected.tabnav-tab')[0];
-  selectedTab.classList.remove("selected");
-  
-  var buildLink = window.document.getElementById('travisBuildLink');
-  buildLink.classList.add("selected");
-
-  var travisContainer = window.document.createElement('div');
-  travisContainer.id = 'travisContainer';
-  var table = window.document.createElement('table');
-  var tableHead = window.document.createElement('thead');
-  var tableHeadRow = window.document.createElement('tr');
-  var tableHeadCol1 = window.document.createElement('td');
-  tableHeadCol1.innerHTML = 'Build';
-  tableHeadRow.appendChild(tableHeadCol1);
-
-  var tableHeadCol2 = window.document.createElement('td');
-  tableHeadCol2.innerHTML = 'Branch';
-  tableHeadRow.appendChild(tableHeadCol2);
-  
-  var tableHeadCol3 = window.document.createElement('td');
-  tableHeadCol3.innerHTML = 'Message';
-  tableHeadRow.appendChild(tableHeadCol3);
-  
-  var tableHeadCol4 = window.document.createElement('td');
-  tableHeadCol4.innerHTML = 'Duration';
-  tableHeadRow.appendChild(tableHeadCol4);
-  
-  var tableHeadCol5 = window.document.createElement('td');
-  tableHeadCol5.innerHTML = 'Finished';
-  tableHeadRow.appendChild(tableHeadCol5);
-
-  tableHead.appendChild(tableHeadRow)
-  table.appendChild(tableHead);
-  travisContainer.appendChild(table);
-  var containerParent = container.parentNode;
-  containerParent.insertBefore(travisContainer, container);
-  
-  createAddonStyles();
-
-  var xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      //TODO(David) Add in the building the rest of the table
-      alert(xhr.responseText)
-    }
-  }
-  xhr.open("GET", "https://api.travis-ci.org/builds/", true);
-  xhr.send();
-})
-
-// Adding the link
-var buildLink = window.document.createElement('a');
-buildLink.id = 'travisBuildLink';
-buildLink.href = '#';
-buildLink.innerHTML = "Builds";
-buildLink.classList.add('tabnav-tab');
-buildTab.appendChild(buildLink);
-
-// Attaching to the DOM
-var tabs = window.document.getElementsByClassName('tabnav-tab');
-if (tabs){
-  var parentEl = tabs[4].parentNode;
-  parentEl.parentNode.insertBefore(buildTab, tabs[4].parentNode.nextSibling);
-}
-
-var createAddonStyles = function() {
-  var travisStyle = window.document.createElement('style');
-  travisStyle.innerHTML = 'th { font-size: 13px; color: rgb(102,102,102); }';
-  travisStyle.innerHTML += 'td, th { padding: 5px 10px;';
-  travisStyle.innerHTML += 'border-bottom: 2px solid white; }';
-  var body = window.document.getElementsByTagName('body')[0];
-  body.appendChild(travisStyle);
-};
