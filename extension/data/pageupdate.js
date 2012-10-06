986818d112c4429427067ee6301cdd1c636c3842
AutomatedTester <dburns@mozilla.com>
2012-10-06 16:04:14 +0100

    add in the beginnings of the build data table

--- extension/data/pageupdate.js
+++ extension/data/pageupdate.js
@@ -7,7 +7,7 @@ style.innerHTML += '#travis-ci img{display:block;}';
 var tab = window.document.getElementsByClassName('title-actions-bar')[0];
 var h1 = tab.getElementsByTagName('h1')[0];
 var strong = h1.getElementsByTagName('strong')[0];
-          
+
 var project = window.location.pathname.split('/').splice(0,3).join('/');
 var img = window.document.createElement('img');
             
@@ -35,7 +35,45 @@ buildTab.addEventListener('click', function(){
   var buildLink = window.document.getElementById('travisBuildLink');
   buildLink.classList.add("selected");
 
-  //TODO(David) Add code to pull build status from Travis API
+  var travisContainer = window.document.createElement('div');
+  travisContainer.id = 'travisContainer';
+  var table = window.document.createElement('table');
+  var tableHead = window.document.createElement('thead');
+  var tableHeadRow = window.document.createElement('tr');
+  var tableHeadCol1 = window.document.createElement('td');
+  tableHeadCol1.innerHTML = 'Build';
+  tableHeadRow.appendChild(tableHeadCol1);
+
+  var tableHeadCol2 = window.document.createElement('td');
+  tableHeadCol2.innerHTML = 'Branch';
+  tableHeadRow.appendChild(tableHeadCol2);
+  
+  var tableHeadCol3 = window.document.createElement('td');
+  tableHeadCol3.innerHTML = 'Message';
+  tableHeadRow.appendChild(tableHeadCol3);
+  
+  var tableHeadCol4 = window.document.createElement('td');
+  tableHeadCol4.innerHTML = 'Duration';
+  tableHeadRow.appendChild(tableHeadCol4);
+  
+  var tableHeadCol5 = window.document.createElement('td');
+  tableHeadCol5.innerHTML = 'Finished';
+  tableHeadRow.appendChild(tableHeadCol5);
+
+  tableHead.appendChild(tableHeadRow)
+  table.appendChild(tableHead);
+  travisContainer.appendChild(table);
+  var containerParent = container.parentNode;
+  containerParent.insertBefore(travisContainer, container);
+  createAddonStyles();
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      //TODO(David) Add in the building the rest of the table
+    }
+  }
+  xhr.open("GET", "http://travis-ci.org/" + project + "/builds.json", true);
+  xhr.send();
 })
 
 // Adding the link
@@ -51,3 +89,11 @@ var tabs = window.document.getElementsByClassName('tabnav-tab');
 var parentEl = tabs[4].parentNode;
 parentEl.parentNode.insertBefore(buildTab, tabs[4].parentNode.nextSibling);
 
+var createAddonStyles = function() {
+  var travisStyle = window.document.createElement('style');
+  travisStyle.innerHTML = 'th { font-size: 13px; color: rgb(102,102,102); }';
+  travisStyle.innerHTML += 'td, th { padding: 5px 10px;';
+  travisStyle.innerHTML += 'border-bottom: 2px solid white; }';
+  var body = window.document.getElementsByTagName('body')[0];
+  body.appendChild(travisStyle);
+};
